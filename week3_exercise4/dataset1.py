eb4c75ebbcb6ee04c32af3e036bb6f5b875a2bae
TachikawaKaito <e245304@ie.u-ryukyu.ac.jp>
2026-04-28 14:16:31 +0900

    演習1.2

--- week3_exercise4/dataset1.py
+++ week3_exercise4/dataset1.py
@@ -1,4 +1,19 @@
 import numpy as np
+import pandas as pd
 
 def true_function(x):
     return np.sin(np.pi * x * 0.8) * 10
+
+def random_x():
+    np.random.seed(0)
+    return np.random.uniform(-1, 1, 20)
+
+def create_datasets():
+    x = random_x()
+    y = true_function(x)
+
+    df = pd.DataFrame({
+        "観測値" : x,
+        "真値" : y,
+    })
+    return df
--- week3_exercise4/dataset1_graph.py
+++ week3_exercise4/dataset1_graph.py
@@ -5,9 +5,13 @@ import numpy as np
 x = np.linspace(-1, 1, 100)
 y = dataset1.true_function(x)
 
+df = dataset1.create_datasets()
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.plot(x, y, color="blue", label="y = sin(pi * x * 0.8) * 10")
-plt.legend(loc="best")
+plt.scatter(df["観測値"], df["真値"], color="red", label="samples")
+
+plt.legend()
 plt.show()
 
