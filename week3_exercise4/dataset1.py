f3c4314f205227e809c341bc2c028ceb37b2928f
TachikawaKaito <e245304@ie.u-ryukyu.ac.jp>
2026-05-12 10:54:02 +0900

    演習1.3

--- week3_exercise4/dataset1.py
+++ week3_exercise4/dataset1.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import unittest
+import japanize_matplotlib
+
 
 def true_function(x):
     return np.sin(np.pi * x * 0.8) * 10
@@ -57,5 +59,27 @@ if __name__ == "__main__":
     plt.scatter(df["観測点"], df["真値"], color="red", label="samples")
     plt.legend()
     plt.savefig("week3_exercise4/ex1.2.png")
+
+    plt.show()
+
+    # 演習1.3
+
+    x = np.linspace(-1, 1, 100)
+    y = true_function(x)
+
+    df = create_datasets()
+
+    noise = np.random.normal(loc=0.0, scale=np.sqrt(2.0), size=20)
+    noise_half = noise * 0.5
+
+    df["観測値"] = df["真値"] + noise_half
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(x, y, color="blue", label=r"$y = \sin(0.8 \pi x) \times 10$")
+    plt.scatter(df["観測点"], df["真値"], color="red", label="samples")
+    plt.scatter(df["観測点"], df["観測値"], color="green", label="観測値")
+    plt.legend()
+    plt.savefig("week3_exercise4/ex1.3.png")
 
     plt.show()
